633bc3618b90e172c1b6414521c1385e6882a0db
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-05-23 16:27:14 +0200

    Erstellen des WPF Projektes, UnitTest Projektes und einer Klassenbibliothek. Objekte mittels EF Core Powertool in der Bibliothek erstellt.

---
+++ GymlerUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GymlerUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
