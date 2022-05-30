f0eaabb1f313acbe128e93eb9c1c7712822e952a
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-05-30 16:25:59 +0200

    ersten 2 routen
    - GetUebungenByNutzer
    - GetPlaeneByNutzer

---
+++ GymlerWebAPI/Controllers/GymlerController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GymlerLibrary.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace GymlerWebAPI.Controllers
+{
+    [ApiController]
+    public class GymlerController : ControllerBase
+    {
+        public static gymlerjovanovicmContext context = new gymlerjovanovicmContext();
+
+
+        [HttpGet("uebungen/{uID}")]
+        public ActionResult<List<Uebung>> GetUebungenByNutzer(int uID)
+        {
+            var nutzerInnen = context.NutzerIn;
+            var uebungen = context.Uebung;
+
+            var nutzerByID = nutzerInnen.Where(n => n.Id == uID).FirstOrDefault();
+
+
+            var ubeungenByNutzer = uebungen.SelectMany(u => u.FuehrtDurch).Where(fd => fd.IdNavigation == nutzerByID).ToList();
+
+
+            Console.WriteLine($"Getting Uebungen from NutzerIn {nutzerByID.Nutzername}...");
+
+            if (nutzerByID == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(uebungen);
+
+            }
+
+
+        }
+
+
+        [HttpGet("plaene/{uID}")]
+        public ActionResult<List<Trainingsplan>> GetPlaeneByNutzer(int uID)
+        {
+            var nutzerInnen = context.NutzerIn;
+            var nutzerByID = nutzerInnen.Where(n => n.Id == uID).FirstOrDefault();
+
+
+            var plaeneByNutzer = nutzerByID.Trainingsplan.ToList();
+
+
+            Console.WriteLine($"Getting Plaene from NutzerIn {nutzerByID.Nutzername}...");
+
+            if (nutzerByID == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(plaeneByNutzer);
+
+            }
+
+
+        }
+
+
+
+
+    }
+
+}
