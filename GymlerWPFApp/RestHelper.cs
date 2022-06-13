d132b4522e46fd7a4d5c2384e218a117ff6de342
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-06-13 16:16:02 +0200

    rest helper erstellz

--- GymlerWPFApp/RestHelper.cs
+++ GymlerWPFApp/RestHelper.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 
 namespace GymlerWPFApp
 {
     internal class RestHelper
     {
+        private static HttpClient client = new HttpClient() { BaseAddress = new Uri("https://localhost:2682/api/") };
+        static JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
 
+
+        
 
 
     }
